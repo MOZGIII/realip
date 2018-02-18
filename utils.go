724469818f56eafd97908995825f1d1177efd0fc
MOZGIII <mike-n@narod.ru>
2018-02-19 02:11:07 +0300

    Removed useless and poorly written comment

--- utils.go
+++ utils.go
@@ -6,8 +6,6 @@ import (
 )
 
 func ipFromRemoteAddr(remoteAddr string) (string, error) {
-	// If there are colon in remote address, remove the port number
-	// otherwise, return remote address as is.
 	if strings.ContainsRune(remoteAddr, ':') {
 		remoteIP, _, err := net.SplitHostPort(remoteAddr) // noling: gas
 		if err != nil {
